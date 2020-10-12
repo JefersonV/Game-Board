1aa2d1be5c719c971e4e4c3052bae8197ee7df8e
Jeferson Velasquez <jvelasquezc15@miumg.edu.gt>
2020-10-12 17:50:38 -0600

    03 iluminación de colores -> 5 métodos creados y classList utilizado

--- game.js
+++ game.js
@@ -1,23 +1,66 @@
 const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
+      const violeta = document.getElementById('violeta')
+      const naranja = document.getElementById('naranja')
+      const verde = document.getElementById('verde')
+      const btnEmpezar = document.getElementById('btnEmpezar')
 
-class Juego {
-  constructor() {
-    this.inicializar()
-    this.generarSecuencia()
-  }
+      class Juego {
+        constructor() {
+          this.inicializar()
+          this.generarSecuencia()
+          this.siguienteNivel()
+        }
 
-  inicializar() {
-    btnEmpezar.classList.add('hide')
-  }
-  generarSecuencia() {
-    this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4 ))
-  }
-}
+        inicializar() {
+          btnEmpezar.classList.add('hide')
+          this.nivel = 1
+          this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+          }
+        }
 
-function empezarJuego() {
-  window.juego = new Juego()
-}
+        generarSecuencia() {
+          this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+        }
+
+        siguienteNivel() {
+          this.iluminarSecuencia()
+        }
+
+        transformarNumeroAColor(numero) {
+          switch (numero) {
+            case 0:
+              return 'celeste'
+            case 1:
+              return 'violeta'
+            case 2:
+              return 'naranja'
+            case 3:
+              return 'verde'
+          }
+        }
+
+        iluminarSecuencia() {
+          for (let i = 0; i < this.nivel; i++) {
+            const color = this.transformarNumeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
+          }
+        }
+
+        iluminarColor(color) {
+          this.colores[color].classList.add('light')
+          setTimeout(() => this.apagarColor(color), 350)
+        }
+
+        apagarColor(color) {
+          this.colores[color].classList.remove('light')
+        }
+      }
+
+      function empezarJuego() {
+        /* Para debuggear -> window */
+        window.juego = new Juego()
+      }
